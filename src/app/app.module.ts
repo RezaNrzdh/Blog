c9d1df26cd486f5c886cfa27097f6d636d43af2e
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-10-18 22:16:39 +0330

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgetComponent } from './auth/forget/forget.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { RoutingModule } from './routing.module';
+import { ButtonComponent } from './shared/button/button.component';
+import { TextboxComponent } from './shared/textbox/textbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    TextboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,4 +1,26 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { AboutComponent } from "./about/about.component";
+import { ArticleComponent } from "./article/article.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { ContactComponent } from "./contact/contact.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent  },
+    { path: 'article', component: ArticleComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent } 
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
 export class RoutingModule {
-
+    
 }
