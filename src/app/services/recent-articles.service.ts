ba3a3e4728dcbbd8d3fc7b1b10f68208a8911541
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-10-20 00:42:19 +0330

    service

--- src/app/services/recent-articles.service.ts
+++ src/app/services/recent-articles.service.ts
@@ -2,25 +2,21 @@ import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
 export class RecentArticlesService {
-    private data: object[] = [
+    public data: object[] = [
         {
-            img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/61/40/c2/caption.jpg?w=300&h=300',
+            img: '/assets/images/recent_1_300.jpg',
             title: 'چطور از سفر به شهر زیبای لندن لذت ببریم؟',
             created: '10 شهریور 1401'
         },
         {
-            img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/61/28/44/caption.jpg?w=300&h=300',
+            img: '/assets/images/recent_2_300.jpg',
             title: 'دیدنی ترین مکان هایی که میتوانید در ماه دسامبر به آنجا بروید',
             created: '10 شهریور 1401'
         },
         {
-            img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/4a/a6/a6/caption.jpg?w=300&h=300',
+            img: '/assets/images/recent_3_300.jpg',
             title: '15 کار سرگرم کننده و منحصر به فرد که فقط در سن آنتونیو میتوانید انجام دهید',
             created: '10 شهریور 1401'
         }
     ];
-
-    getArticles = (): object[] => {
-        return this.data;
-    }
 }
