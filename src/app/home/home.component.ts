8accf0008f61a744607069e60df36cdae29e7697
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-10-31 01:22:19 +0330

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { EditorPickedService } from '../services/editor-picked.service';
-import { LatestArticlesService } from '../services/latest-articles-service';
 
-import { RecentArticlesService } from '../services/recent-articles.service';
+import { HomeService } from '../services/home.service';
+
+interface RecentArticles {
+  img: string;
+  title: string;
+  author: string;
+  created: string
+}
+
+interface EditorPicked {
+  img: string;
+  title: string;
+  created: string;
+}
+
+interface OtherLastetArticles {
+  img: string;
+  title: string;
+  created: string;
+}
+
+interface LatestArticle {
+  img: string;
+  title: string;
+  desc: string;
+  authorImg: string;
+  author: string;
+  duration: string;
+  created: string;
+}
 
 @Component({
   selector: 'blog-home',
@@ -10,20 +37,18 @@ import { RecentArticlesService } from '../services/recent-articles.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  latestArticle: LatestArticle = { img: '', title: '', desc: '', authorImg: '', author: '', duration: '', created: '' };
+  otherLatestArticles: Array<OtherLastetArticles> = [];
+  editorPicked: Array<EditorPicked> = [];
+  recentArticles: Array<RecentArticles> = [];
 
-  latestArticlesData: any = {};
-  recentArticlesData: any = [];
-  editorPickedData: any = [];
-
-  constructor(
-    private recentArticles: RecentArticlesService,
-    private latestArticles: LatestArticlesService,
-    private editorPicked: EditorPickedService) { }
+  constructor(private homeservice: HomeService) { }
 
   ngOnInit(): void {
-    this.latestArticlesData = this.latestArticles.data;
-    this.recentArticlesData = this.recentArticles.data;
-    this.editorPickedData = this.editorPicked.data;
+    this.latestArticle = this.homeservice.getLatestArticle();
+    this.otherLatestArticles = this.homeservice.getOtherLatestArticles();
+    this.editorPicked = this.homeservice.getEditorPicked();
+    this.recentArticles = this.homeservice.getRecentArticles();
   }
 
 }
---
+++ src/app/services/home.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import data from "../../assets/dummy-data.json";
+
+@Injectable({ providedIn: 'root' })
+export class HomeService {
+    getEditorPicked = () => {
+        return data.editorPicked;
+    }
+
+    getLatestArticle = () => {
+        return data.latestArticle;
+    }
+
+    getOtherLatestArticles = () => {
+        return data.otherLatestArticles;
+    }
+
+    getRecentArticles = () => {
+        return data.recentArticles;
+    }
+}
