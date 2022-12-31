fa3e81921849d85ed538f7fd09ffa6900a61474d
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-01-01 00:20:39 +0330

    update

--- src/app/core/models/about.model.ts
+++ src/app/core/models/about.model.ts
@@ -1,4 +1,5 @@
 export class AboutModel {
     public goals?: string;
     public properties?: Array<{ img: string; title: string; desc: string; }>;
+    public team?: Array<{ img: string, name: string, desc: string }>;
 }
