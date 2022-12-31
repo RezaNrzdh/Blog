52b45c4d0881b4137339b749852e1b98aa2c130b
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2022-12-31 22:57:01 +0330

    change about page

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -5,16 +5,18 @@ import { AboutModel } from "../../core/models/about.model";
 @Component({
     selector: 'blog-about',
     templateUrl: './about.component.html',
+    providers: [AboutService],
     styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-    data?: AboutModel;
+    data: AboutModel = new AboutModel();
 
     constructor(private aboutService: AboutService) { }
 
     ngOnInit(): void {
-        this.data = this.aboutService;
+        this.data = this.aboutService.getAllData();
+        console.log(this.data);
     }
 
 }
