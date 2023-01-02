3a8c4db6d98d418dcbf51fcede2c5df4729d9fb9
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-01-02 11:37:41 +0330

    add newsletter to the home page

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {HomeRoutingModule} from "./home-routing.module";
+
 import { HomeComponent } from './home.component';
 import { NewestComponent } from './components/newest/newest.component';
 import { PickedComponent } from './components/picked/picked.component';
 import { RecentComponent } from './components/recent/recent.component';
-import {HomeRoutingModule} from "./home-routing.module";
+import { NewsletterComponent } from "./components/newsletter/newsletter.component";
 
 @NgModule({
     declarations: [
         HomeComponent,
         NewestComponent,
         PickedComponent,
-        RecentComponent
+        RecentComponent,
+        NewsletterComponent
     ],
     imports: [
         CommonModule,
