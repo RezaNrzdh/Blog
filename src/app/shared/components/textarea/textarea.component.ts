7979c83b393a4fa0e2a776f20332014ee77df386
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-02-12 12:46:09 +0330

    update textarea component

--- src/app/shared/components/textarea/textarea.component.ts
+++ src/app/shared/components/textarea/textarea.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from "@angular/core";
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {AlertmessageEnum} from "../../../core/enum/alertmessage.enum";
 
 @Component({
     selector: "blog-textarea",
@@ -16,14 +17,18 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 export class TextareaComponent implements ControlValueAccessor {
 
     @Input() title: string = "";
+    @Input() name: string = "";
     @Input() placeholder: string = "";
     @Input() maxLength: number = 150;
     @Input() rows: number = 4;
     @Input() width: string = "100%";
+    @Input() alertMessage: string = "";
+    @Input() formGroup: any;
 
     value!: string;
     onChange!: (value: any) => void;
     onTouched!: () => void;
+    alertmsg: any = AlertmessageEnum;
 
     constructor() {
     }
