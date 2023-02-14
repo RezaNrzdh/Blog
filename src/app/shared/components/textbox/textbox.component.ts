b75f45a70ed3315ca0b50108960c942b0b35e25b
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-02-14 10:16:57 +0330

    fixed textbox component

--- src/app/shared/components/textbox/textbox.component.ts
+++ src/app/shared/components/textbox/textbox.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input } from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 import { AlertmessageEnum } from "src/app/core/enum/alertmessage.enum";
 
